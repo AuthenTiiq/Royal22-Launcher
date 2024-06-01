10149f04a712da252a6250f4e4ec868518e38516
AuthenTiiq <47777491+AuthenTiiq@users.noreply.github.com>
2024-06-01 21:46:57 +0200

    add text splash

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,11 +30,12 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je vois quelque chose en direction de.. RC", "author": "Explorateur" },+
+            { "message": "Je vois quelque chose en direction de...", "author": "Explorateur" },+
             { "message": "Et si ce n'était qu'un projet...", "author": "Explorateur" },
             { "message": "Je le vois ! Oui ! C'est le Royaume !", "author": "Explorateur" },
             { "message": "Et si RoyalCreep's...", "author": "Inconnu" },
-            { "message": "RoyalCreep's est née en 2013...", "author": "Inconnu" }
+            { "message": "RoyalCreep's est née en 2013...", "author": "Inconnu" },
+            { "message": "Rapidité...", "author": "Inconnu" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
